2e7e1e9cd9a078442bc5c722d9d0eb8e58d7a2ca
Brian Mathews <spoonart@gmail.com>
2013-08-30 15:55:44 -0700

    ui improvments. response parsing improvements. bumped xml2js version

--- ui/js/message.js
+++ ui/js/message.js
@@ -4,16 +4,24 @@ function MessageController($scope, $rootScope, $sce) {
         var accounts = require('./../lib/accounts').getAccounts(),
             api = accounts['atsid'].api;
 
-        
+        $rootScope.$on('selectedFolder', function () {
+            $scope.email = null;
+        });
         $rootScope.$on('selectedEmail', function (e, email) {
+            $scope.email = null;
             api.getEmailById(email.id, function (err, email) {
                 $scope.$apply(function () {
                     if (err) {
                         $rootScope.showError(err);
                     } else {
-                        email.body = email.body.substr(email.body.indexOf("<body"));
-                        email.body = email.body.substr(email.body.indexOf('>') + 1);
-                        email.body = $sce.trustAsHtml(email.body.substr(0, email.body.indexOf("</body>")));
+                        if (email.body.indexOf("</body>") > -1) {
+                            email.body = email.body.substr(email.body.indexOf("<body"));
+                            email.body = email.body.substr(email.body.indexOf('>') + 1);
+                            email.body = email.body.substr(0, email.body.indexOf("</body>"));
+                        } else {
+                            email.body = '<div class="plain">' + email.body + '</div>';
+                        }
+                        email.body = $sce.trustAsHtml(email.body);
                         $scope.email = email;
                     }
                 });
